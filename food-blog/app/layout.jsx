e03ba672f78cc04da2d272117e8c9ea9e3b0443c
njenkins2727 <njenkins2727@gmail.com>
2024-03-31 10:50:13 +0800

    getting to know tailwind and daisy ui

--- food-blog/app/layout.jsx
+++ food-blog/app/layout.jsx
@@ -1,7 +1,7 @@
 import '@styles/global.css'
 
 export const metadata = {
-    title: "gastronomic-alchemy",
+    title: "Title text",
     description: 'Discover and Save amazing recipes'
 }
 
@@ -9,16 +9,7 @@ const RootLayout = ({ children }) => {
     return (
       <html lang='en'>
           <body>
-            {/* <Provider>
-              <div className='main'>
-                  <div className='gradient' />
-              </div>
-  
-              <main className='app'>
-                <Nav/>
-               {children}
-              </main>
-            </Provider> */}
+            {children}
           </body>
       </html>
     )
--- food-blog/app/page.jsx
+++ food-blog/app/page.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import Register from '@components/register'
 const Home = () => {
   return (
-    <div>page</div>
+    <main>
+      {/* Goal: use tailwind css daisy ui to make a good looking login form registry */}
+      <Register/>
+    </main>
   )
 }
 
