69f9d1b604693919193b2c1bb33de480346fcaaa
njenkins2727 <njenkins2727@gmail.com>
2024-04-07 12:38:21 +0800

    added footer component

--- food-blog/app/page.jsx
+++ food-blog/app/page.jsx
@@ -1,12 +1,14 @@
 import Hero from '@components/Hero'
 import Nav from '../components/Nav'
 import About from '@components/About'
+import Footer from '@components/Footer'
 const Home = () => {
   return (
     <section className="main">
       <Nav/>
       <Hero/>
       <About/>
+      <Footer/>
     </section>
   )
 }
