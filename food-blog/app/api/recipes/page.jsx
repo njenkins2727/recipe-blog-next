bd64b9e3745abfe5069cf293569a87e443b4d72e
njenkins2727 <njenkins2727@gmail.com>
2024-04-08 19:18:30 +0800

    move recipes to api folder and remove bug/issues

---
+++ food-blog/app/api/recipes/page.jsx
@@ -0,0 +1,15 @@
+
+// import Recipe from "@models/recipe";
+
+// export const GET = async (request) => {
+//   try {
+//       await connectToDB();
+
+//       const recipe = await Recipe.find({}).populate('creator');
+
+//       return new Response(JSON.stringify(recipe), { status: 201 })
+//   } catch (error){
+//      return new Response('Failed to fetch all recipe', { status: 500 })
+//   }
+// }
+
