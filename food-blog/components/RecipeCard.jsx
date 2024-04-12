65369af1aa3e989c4a56f3dcfa432f08a4b80649
njenkins2727 <njenkins2727@gmail.com>
2024-04-12 21:23:10 +0800

    edit files, started retrieving data from db

--- food-blog/components/RecipeCard.jsx
+++ food-blog/components/RecipeCard.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 
 const RecipeCard = ({ title, desc, image, ingredients, method }) => {
+//   const { MongoClient } = require("mongodb");
+// // Replace the uri string with your MongoDB deployment's connection string.
+// const client = new MongoClient(process.env.MONGODB_URI);
+// async function run() {
+//   try {
+//     await client.connect();
+//     // database and collection code goes here
+//     const db = client.db("onebusyweek_data")
+//     const coll = db.collection("recipes");
+//     // find code goes here
+//     const cursor = coll.find();
+//     // iterate code goes here
+    
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
   return (
     // do i need to make img src -> img url(image address)
     <div className="card lg:card-side bg-base-100 shadow-xl">
-        {/* <figure><img src={img} alt={imgAlt}/></figure> 
-        <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <p>{desc}</p>
-            <div className="card-actions justify-end">
-                {data}
-            </div>
-        </div> */}
-        Card
+        {/* <h1>{title}</h1> */}
+        {/* <p>{desc}</p> */}
+        {/* <p>{image}</p> */}
+        {/* <p>{ingredients}</p> */}
+        {/* <p>{method}</p> */}
+        Cards
     </div>
   )
 }
