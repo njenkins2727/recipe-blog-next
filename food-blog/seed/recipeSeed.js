bc040acd4ee55ab9760965bab09a5e8fff3de076
njenkins2727 <njenkins2727@gmail.com>
2024-04-17 17:37:05 +0800

    added more recipes in seed, have not re-seeded to database yet

--- food-blog/seed/recipeSeed.js
+++ food-blog/seed/recipeSeed.js
@@ -1,7 +1,7 @@
 const recipeData = [
     {
-        title: 'Burito bowls',
-        desc: 'tasty easy weeknight meal',
+        title: 'Burito Bowls',
+        desc: 'Tasty easy weeknight meal.',
         image: 'https://images.unsplash.com/photo-1668665771757-4d42737d295a?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         ingredient: [
             'Rice',
@@ -10,7 +10,8 @@ const recipeData = [
             'Avocado',
             'Red Onion',
             'Lime',
-            'Tomato'
+            'Tomato',
+            'Lettuce',
         ],
         method: [
             'Wash the rice thoroughly and start to cook.',
@@ -19,5 +20,46 @@ const recipeData = [
             'Serve over the rice and add toppings of your choice. Enjoy!',
         ],
     },
+    {
+        title:`Lola's Pork Adobo`,
+        desc:'The key is the marinate the pork overnight.',
+        image:'https://images.unsplash.com/photo-1612156723470-10f426219f63?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        ingredient: [
+            'Rice',
+            'Pork rashers',
+            'Garlic',
+            'Onion',
+            'Vinegar',
+            'Soy sauce',
+            'Bay leaf',
+            'Lemon grass',
+            'Vegeta'
+        ],
+        method:[
+
+        ],
+    },
+    {
+        title: 'Lamb Saganaki',
+        desc:'A meal using everyday ingredients in a new and inspired way - meatballs are a guaranteed family pleaser.',
+        image:'https://images.unsplash.com/photo-1515516969-d4008cc6241a?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        ingredient: [
+            'Lamb mince',
+            'Risoni',
+            'Breadcrumbs',
+            'Egg',
+            'Oregano',
+            'Onion',
+            'Garlic',
+            'Dry white wine',
+            'Diced tomatoes',
+            'Beef stock',
+            'Parsley',
+            'Feta and Parsley (to serve)',
+        ],
+        method:[
+
+        ],
+    }
 ];
 export default recipeData;
