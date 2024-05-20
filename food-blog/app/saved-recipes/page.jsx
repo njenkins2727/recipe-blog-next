b1bbb29b5e3e372a9b01769a6637848469125df9
njenkins2727 <njenkins2727@gmail.com>
2024-05-20 20:45:23 +0800

    bug fix with saved-recipes page

--- food-blog/app/saved-recipes/page.jsx
+++ food-blog/app/saved-recipes/page.jsx
@@ -5,21 +5,17 @@ import Footer from '../../components/Footer'
 import { useSession } from "next-auth/react"
 import RecipeCard from '../../components/RecipeCard'
 
+// useState called insital state first run which returns said initial state to the RecipeCard component which needs to render the data. 
 
 const Saved = () => {
   const { data: session } = useSession();
   const userId = session?.user.id
-  const [recipe, setRecipe] = useState([]);
-
-// Get saved recipe Ids from localStorage (saved in array of userId)
-// fetch Recipes by Id
-// make function of list of recipe cards that similar to feed.jsx 
-// display looped recipes through saved-recipes 
-//works
+  const [recipe, setRecipe] = useState([]); //initial state is an empty array 
 
 const loadSavedData = useCallback(async () => {
   try {
     let recipeIdArr = JSON.parse(localStorage.getItem(userId));
+    // console.log(recipeIdArr); //saved recipes _id
     let arr = [];
     
     recipeIdArr.map(dataId => {
@@ -27,10 +23,12 @@ const loadSavedData = useCallback(async () => {
         const response = await fetch(`/api/recipes/${dataId}`);
         const data = await response.json();
         arr.push(data);
+        // console.log(data); //sepereate objects of the saved recipes full data -- however, renders twice, back-to-back
       }
       fetchRecipes();
       setRecipe(arr);
     });
+    // console.log(arr); // array of obove objects -- loads three times, twice. 
     
     
   } catch (error) {
@@ -43,14 +41,15 @@ useEffect(() => {
 }, [loadSavedData]);
 
 const RecipeCardList = ({ data }) => {
+  console.log(data); //returns empty array twice, then returns full array twice 
   return (
     <div className='flex flex-row flex-wrap'>
-      {data.map((recipe => (
+      {/* {data.map((recipe => (
         <RecipeCard
         key={recipe._id}
         data={recipe}
         />
-      )))} 
+      )))}  */}
     </div>
   )
 }
@@ -59,11 +58,11 @@ const RecipeCardList = ({ data }) => {
     <Nav/>
     <h1>Saved Recipes</h1>
 
-    <RecipeCardList //command fires - but doesnt display. Invisible? 
+    <RecipeCardList //command fires - but doesnt display on initial render. on refresh there is unexpected json input err. when i leave window and return it re render properly.. (useEffect, useCallback?)
     data={recipe}
     />
     
-
+{/* {console.log(recipe)} */}
     <Footer/>
     </div>
   )
